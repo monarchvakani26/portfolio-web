e5edd7e5705a7f6fd7f93edc6c69874ab19ff8f0
monarchvakani26 <160637766+monarchvakani26@users.noreply.github.com>
2024-09-01 12:19:19 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,5 @@
+function family(){
+    let familyMembers = ["Monarch","Niomi","Parul","Harshad"]
+    
+    document.getElementById("demo").innerHTML = familyMembers[0] + " " + familyMembers.length;
+}
